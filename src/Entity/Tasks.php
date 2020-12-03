d55b8e022b5fecda26db87eb561b9251c91a1fc3
ericc70 <eric.c@codeur.online>
2020-12-03 16:02:14 +0000

    first test api-plateform

---
+++ src/Entity/Tasks.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\TasksRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource(
+ *  collectionOperations={"get"},
+ * itemOperations={"get"}
+ * )
+ * @ORM\Entity(repositoryClass=TasksRepository::class)
+ */
+class Tasks
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateAt;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $dateFor;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $status;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(?string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getDateAt(): ?\DateTimeInterface
+    {
+        return $this->dateAt;
+    }
+
+    public function setDateAt(\DateTimeInterface $dateAt): self
+    {
+        $this->dateAt = $dateAt;
+
+        return $this;
+    }
+
+    public function getDateFor(): ?\DateTimeInterface
+    {
+        return $this->dateFor;
+    }
+
+    public function setDateFor(?\DateTimeInterface $dateFor): self
+    {
+        $this->dateFor = $dateFor;
+
+        return $this;
+    }
+
+    public function getStatus(): ?int
+    {
+        return $this->status;
+    }
+
+    public function setStatus(int $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
